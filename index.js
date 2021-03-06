b4fb498df9121f3ec4ee4298c8021b01529b6c49
Sergey <karandeev73@gmail.com>
2021-03-06 12:18:59 +0300

    feat: add about page

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
 
 const server = http.createServer((req, res) => {
-   if(req.method === 'GET')    {
+    if (req.method === 'GET') {
         res.writeHead(200, {
-            'Content-Type': 'text/html'
+            'Content-Type': 'text/html; charset=utf-8'
         })
 
-       res.end(`
-       <h1>Form</h1>
-       <form method="post" action="/" >
-        <input name="title" type="text" />
-        <button type="sumbit">Send</button>
-       </form>
-       `)
-   } else if (req.method ==='POST') {
+        if (req.url === '/') {
+            fs.readFile(path.join(__dirname, 'views', 'index.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err;
+                    }
+                    res.end(content);
+                }
+            )
+        } else if (req.url==='/about') {
+            fs.readFile(path.join(__dirname, 'views', 'about.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err;
+                    }
+                    res.end(content);
+                }
+            )
+
+        }
+
+
+    } else if (req.method === 'POST') {
         const body = [];
         res.writeHead(200, {
             'Content-Type': 'text/html; charset=utf-8 '
@@ -24,16 +44,16 @@ const server = http.createServer((req, res) => {
         })
 
         req.on('end', () => {
-           const message = body.toString().split('=')[1]
-         
-           res.end(`
+            const message = body.toString().split('=')[1]
+
+            res.end(`
            <h1>Ваше сообщение:${message} </h1>
         `)
         })
-       
-   }
+
+    }
 })
 
-server.listen(3000, () =>{
+server.listen(3000, () => {
     console.log('Server is running...')
 })
